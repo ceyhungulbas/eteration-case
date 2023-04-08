46fd7d12d7ce2bdef9fb43ecb604567e68a8a46e
ceyhungulbas <cetree61@gmail.com>
2023-04-08 15:59:57 +0300

    feat: add status constants

---
+++ src/constants/status.js
@@ -0,0 +1,7 @@
+export const REDUX_LOADING_STATUS = 'loading';
+
+export const REDUX_REJECTED_STATUS = 'rejected';
+
+export const REDUX_SUCCESS_STATUS = 'success';
+
+export const REDUX_IDLE_STATUS = 'idle';
